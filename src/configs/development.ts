ae4a08d547122db35d36e3afda8326680de49c79
Oyewole ABAYOMI S <oyewoleabayomi@gmail.com>
2020-03-14 02:13:26 +0100

    update .env.example

--- src/configs/development.ts
+++ src/configs/development.ts
@@ -1,6 +1,21 @@
+import { URL } from 'url';
+
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_PORT = process.env.REDIS_PORT || '6379';
+
+const redisUrl = new URL(process.env.REDIS_URL || `redis://${REDIS_HOST}:${REDIS_PORT}/`);
+
 export default {
   general: {
-    port: process.env.PORT || 3000,
-    env: 'development'
+    env: 'development',
+    baseUrl: process.env.BASE_URL,
+    port: process.env.PORT || 3000
+  },
+  redis: {
+    host: redisUrl.hostname || 'localhost',
+    port: parseInt(redisUrl.port || '6379', 10),
+    db: parseInt((redisUrl.pathname || '/0').substr(1) || '0', 10),
+    password: redisUrl.password ? decodeURIComponent(redisUrl.password) : undefined,
+    prefix: 'sam:'
   }
 };
